c96f09165e0e53c530fc6ed67a50472ce995e2c4
wurenjie <wurenjie@koudailc.com>
2019-01-28 18:07:29 +0800

    redistool set coding

--- redis/src/main/java/com/redis/util/pool/RedisTool.java
+++ redis/src/main/java/com/redis/util/pool/RedisTool.java
@@ -1,9 +1,5 @@
 package com.redis.util.pool;
 
-import jdk.internal.util.xml.impl.Input;
-import redis.clients.jedis.Jedis;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -21,23 +17,53 @@ public class RedisTool {
     }
 
     public String get(String key) throws IOException {
-        String command="get "+key+"\r\n";
-        os.write(command.getBytes());
-        os.flush();
-        byte[] bytes =new byte[8*1024];
-        int index=is.read(bytes);
-        ByteArrayOutputStream baos=new ByteArrayOutputStream();
-        baos.write(bytes,0,index);
-        String respStr=new String(baos.toByteArray());
-        if(respStr.startsWith("$-1")){
+        AssertParam(key);
+        String command = "get " + key + "\r\n";
+        sendCommand(command);
+        byte[] resultBytes = getResult();
+        String respStr = new String(resultBytes);
+        if (respStr.startsWith("$-1")) {
             return null;
         }
-        String[] resultArray=respStr.split("\r\n");
+        String[] resultArray = respStr.split("\r\n");
         return resultArray[1];
+    }
+
+    private void sendCommand(String command) throws IOException {
+        os.write(command.getBytes());
+        os.flush();
+    }
+
+    private byte[] getResult() throws IOException {
+        int len = 0;
+        while (len == 0) {
+            len = is.available();
+        }
+        byte[] bytes = new byte[len];
+        is.read(bytes);
+        return bytes;
+    }
+
+    private void AssertParam(String param) {
+        if (null == param) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String set(String key, String value) throws IOException {
+        AssertParam(key);
+        String command = "set " + key + " " + value + "\r\n";
+        sendCommand(command);
+        byte[] bytes=getResult();
+        String resultStr=new String(bytes);
+        int index=resultStr.lastIndexOf("\r\n");
+        return resultStr.substring(1,index);
     }
 
     public static void main(String[] args) throws IOException {
         RedisTool redisTool = new RedisTool("127.0.0.1", 6379);
+        System.out.println(redisTool.get("sun"));
+        System.out.println(redisTool.set("sun","wurenjie2"));
         System.out.println(redisTool.get("sun"));
     }
 }
